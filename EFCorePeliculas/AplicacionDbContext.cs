a081e777fde0f5e74d7457e0201ecb85f28ef4e8
ezequiel-piki <ezequielpiki23@gmail.com>
2023-05-22 00:45:40 -0300

    initial commit

---
+++ EFCorePeliculas/AplicacionDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EFCorePeliculas.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePeliculas
+{
+    public class AplicacionDbContext : DbContext
+    {
+        public AplicacionDbContext (DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Genero>().HasKey(prop => prop.Identificador);
+            modelBuilder.Entity<Genero>().Property(prop => prop.Nombre).HasMaxLength(150).IsRequired();
+        }
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
---
+++ EFCorePeliculas/Entidades/Genero.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCorePeliculas.Entidades
+{
+    public class Genero
+    {
+        public int Identificador { get; set; }
+
+        // [StringLength(150)]
+        //[MaxLength(150)]
+       // [Required]
+        public string Nombre { get; set;}
+    }
+}
