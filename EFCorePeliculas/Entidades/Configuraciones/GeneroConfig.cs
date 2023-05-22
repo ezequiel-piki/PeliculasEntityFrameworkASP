44280b02154cecd035b90b3cf1e81011cfa3e645
ezequielpiki23 <ezequielpiki23@gmail.com>
2023-05-22 17:54:38 -0300

    ordenando OnModelCreating

---
+++ EFCorePeliculas/Entidades/Configuraciones/GeneroConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace EFCorePeliculas.Entidades.Configuraciones
+{
+    public class GeneroConfig : IEntityTypeConfiguration<Genero>
+    {
+        
+      void IEntityTypeConfiguration<Genero>.Configure(EntityTypeBuilder<Genero> builder)
+        {
+           builder.HasKey(prop => prop.Identificador);
+           builder.Property(prop => prop.Nombre).HasMaxLength(150).IsRequired();
+            // modelBuilder.Entity<Genero>().ToTable(name: "TablaGeneros", schema: "Peliculas");
+        }
+    }
+}
--- EFCorePeliculas/Entidades/Genero.cs
+++ EFCorePeliculas/Entidades/Genero.cs
@@ -1,14 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCorePeliculas.Entidades
 {
+    //[Table("TablaGeneros", Schema = "peliculas")]
     public class Genero
     {
         public int Identificador { get; set; }
 
         // [StringLength(150)]
         //[MaxLength(150)]
-       // [Required]
+        // [Required]
+        //[Column("NombreGenero")]
         public string Nombre { get; set;}
+        public HashSet<Pelicula> peliculas { get; set; }
     }
 }
