c695682ecca17c630609d3aa17b5ca66dfb404d3
ezequielpiki23 <ezequielpiki23@gmail.com>
2023-05-29 17:31:15 -0300

    consultando datos

--- EFCorePeliculas/Servicios/AutoMapperProfiles.cs
+++ EFCorePeliculas/Servicios/AutoMapperProfiles.cs
@@ -13,6 +13,10 @@ namespace EFCorePeliculas.Servicios
             CreateMap<Cine, CineDTO>()
                 .ForMember(dto => dto.Latitud, entidad => entidad.MapFrom(prop => prop.Ubicacion.Y))
                 .ForMember(dto => dto.Longitud, entidad => entidad.MapFrom(prop => prop.Ubicacion.X));
+            CreateMap<Genero, GeneroDTO>();
+            CreateMap<Pelicula, PeliculaDTO>()
+                .ForMember(dto => dto.Cines, ent => ent.MapFrom(prop => prop.SalasDeCine.Select(s => s.Cine)))
+                .ForMember(dto => dto.Actores, ent => ent.MapFrom(prop => prop.PeliculasActores.Select(pa => pa.Actor)));
         }
     }
 }
